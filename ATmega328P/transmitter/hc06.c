cb6ef6851ae0e719b2d7f2194ec5c5a15fa559e8
hugoroldan24 <hugoroldan060@gmail.com>
2026-01-18 10:58:12 +0100

    Cambios HC06

--- ATmega328P/transmitter/hc06.c
+++ ATmega328P/transmitter/hc06.c
@@ -8,26 +8,26 @@ void hc06_send_telemetry(uint8_t *buf)
 {   
    USART_Send_String("Battery Voltage: ");
    
-   sprintf(ascii_str,"%u",buf[0]);
-   USART_Send_String(ascii_str);
+   sprintf(*ascii_str,"%u",buf[0]);
+   USART_Send_String((const char*)ascii_str);
    
    USART_Send_Char('.');
    
-   sprintf(ascii_str,"%u",buf[1]);
-   USART_Send_String(ascii_str);
+   sprintf(*ascii_str,"%u",buf[1]);
+   USART_Send_String((const char*)ascii_str);
    
    USART_Send_String(" V");
    USART_Send_Char('\n');
 
    USART_Send_String("Height: ");
    
-   sprintf(ascii_str,"%u",buf[2]);
-   USART_Send_String(ascii_str);
+   sprintf(*ascii_str,"%u",buf[2]);
+   USART_Send_String((const char*)ascii_str);
    
    USART_Send_Char('.');
    
-   sprintf(ascii_str,"%u",buf[3]);
-   USART_Send_String(ascii_str);
+   sprintf(*ascii_str,"%u",buf[3]);
+   USART_Send_String((const char*)ascii_str);
    
    USART_Send_String(" m");
    
